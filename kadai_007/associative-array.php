0920a58e86a1a7194a0f9f069b885c82b8e9c51a
ioriweb226 <148405516+ioriweb226@users.noreply.github.com>
2023-11-13 13:07:24 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+</head>
+
+<body>
+  <P>
+    <?php
+    // 連想配列に値を代入する
+    $vegetable = ['name'=> 'onion', 'price'=> '200', 'weight' => '160'];
+
+    // 連想配列の値を出力する
+    print_r($vegetable);
+    ?>
+    </p>
+</body>
+</html>
